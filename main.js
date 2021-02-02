ce37c12d71d5e9eae6da2002540b6269bbb632fd
Star X <starx@Star-MacBook-Pro.local>
2021-02-02 15:30:01 -0800

    Steph mostly completed her parts

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+
+function formSubmitEvent() {
+    let title = document.getElementById("title").value;
+    let rating = document.getElementById("rating").value;
+	
+	let mov = new movie(title,rating);
+	//mov.Validate();
+	//document.getElementById("display1").innerHTML = mov.ToString();
+	
+	
+}
--- movie.js
+++ movie.js
@@ -1,7 +1,30 @@
 // JavaScript Document
 
+/* this function was created for testing purposes. Most of it will go in main
+
+function formSubmitEvent() {
+    let title = document.getElementById("title").value;
+    let rating = document.getElementById("rating").value;
+	
+	let mov = new movie(title,rating);
+	mov.Validate();
+	document.getElementById("display1").innerHTML = mov.ToString();
+	
+	
+}
+*/
 var movie = function (title, rating) {
+	
     this.title = title;
     this.rating = rating;
-    this.niceText = function () { return title + "  " + rating }
+    this.ToString = function () { return title + "  " + rating }
+    this.Validate = function(){
+        if(title.length > 2 && rating > 0 && rating <6){
+            return true;
+        }else{  return false;}
+    }
+    
+   
+    
 };
+
