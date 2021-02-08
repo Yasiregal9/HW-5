59dfe715a6d670bddc6b4544f8fdab07d20cd274
ssteph-sudo <77129616+ssteph-sudo@users.noreply.github.com>
2021-02-08 11:19:05 -0800

    Done!

--- main.js
+++ main.js
@@ -1,35 +1,37 @@
 // JavaScript Document
 
 let arr = [];
+let index = 0;
+
 function formSubmitEvent() {
     let title = document.getElementById("title").value;
     let rating = document.getElementById("rating").value;
 	
 	let mov = new movie(title,rating);
 	let good = mov.Validate();
-	
-
 	let concated = title + " " + rating + "\n";
+	
 	if(good){
       arr.push(concated);
-      document.getElementById("display1").innerHTML = mov.ToString();
+      //document.getElementById("display1").innerHTML = mov.ToString();
 	}else{
 		alert("Please fill in all fields. Rating must be between 1 and 5.");
 	}
-	// Why is this here? CustomerInfo();	
 	
 }
 
 function formSubmitEvent2() 
 {
-	let displayMovies = document.getElementById("display");
+	let displayMovies = document.getElementById("display1");
 	displayMovies.innerHTML = "";
+	let str = '<ul>';
 	
-    for(let i=0;i<arr.length;i++)
-    {  		
-		let display = document.createElement("display1");
-        display.appendChild(document.createTextNode(arr[i]));
-		displayMovies.appendChild(display);
+  	for(let i=0;i<arr.length;i++)
+    {  			
+		str += '<li>'+arr[i] +'</li>';
     }
+	
+	
+	displayMovies.innerHTML = str;
 
 }
